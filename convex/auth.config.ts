6dc88d75f46fc97ab1a45bc633e2d1eb68b84fe8
fsRakib <fsrakiba50@gmail.com>
2024-06-06 18:23:02 +0600

    Authenticatin Process Implemented & webhook added

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+  providers: [
+    {
+      domain: "https://intimate-burro-16.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
+
+export default authConfig;
